e934e0124e8935455d962c1de7fc4d3d9295b193
Alireza Ghahramani Kure <alireza.ghahramani@digikala.com>
2023-02-02 20:49:51 +0330

    Qre add apps

--- scripts/screens-monitoring.sh
+++ scripts/screens-monitoring.sh
@@ -15,7 +15,12 @@ time cat ./trace.txt | blkparse -i - > ./parsed_trace.txt
 rm -f ./trace.txt
 
 python3 src/iostat_monitoring/iostat/main.py --data ./iostat.txt --disk $2 --fig-output iostat-plot.png plot
+python3 src/blktrace_monitoring/blktrace_plot.py
+
+chmod -R 777 ./iostat-plot.png
+chmod -R 777 ./blk-4main-plots.png
 
 chmod -R 777 ./iostat.txt
-chmod -R 777 ./iostat-plot.png
 chmod -R 777 ./parsed_trace.txt
+
+
--- src/blktrace_monitoring/blktrace_plot.py
+++ src/blktrace_monitoring/blktrace_plot.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-from collections import Counter
 import seaborn as sns
 import pandas as pd
 
-time_scope_size = 10
-address_scope_size = 25 * 1e6
+from collections import Counter
+from datetime import datetime
+
+fig, ax = plt.subplots(2, 2, frameon=False, figsize=(10, 10))
 
 
 class BLKPlotter:
@@ -30,8 +31,8 @@ class BLKPlotter:
             result.update({'end_address': result['start_address'] + result['size']})
 
             result.update({
-                'scope_start_address': result['start_address'] // address_scope_size,
-                'scope_end_address': result['start_address'] // address_scope_size
+                'scope_start_address': int(result['start_address'] // address_scope_size),
+                'scope_end_address': int(result['start_address'] // address_scope_size)
             })
 
             return result
@@ -44,58 +45,47 @@ class BLKPlotter:
         return df[df.rw_spec != 'N']
 
     @staticmethod
-    def pie_plot(df, save=False):
+    def pie_plot(df):
         palette_color = sns.color_palette('bright')
 
-        plt.pie(
+        ax[0, 0].pie(
             [df[df.rw_spec == 'R'].shape[0], df[df.rw_spec == 'W'].shape[0]],
             labels=['Read', 'Write'],
             colors=palette_color,
             autopct='%.0f%%')
 
-        if save:
-            plt.savefig('blk_rw_pie_plot.png')
-        else:
-            plt.show()
+    @staticmethod
+    def density_on_size(df):
+        ax[0, 1].plot.hist(column=['size'], by='rw_spec', bins=50)
 
     @staticmethod
-    def density_on_size(df, save=False):
-        df.plot.hist(column=['size'], by='rw_spec', bins=50)
-
-        if save:
-            plt.savefig('blk_density_on_size_plot.png')
-        else:
-            plt.show()
-
-    @staticmethod
-    def rw_intensive_plot(df, save=True):
+    def rw_intensive_plot(df):
         def count_R(group):
             return group[group == 'R'].shape[0]
 
         def count_W(group):
             return group[group == 'W'].shape[0]
 
-        dp = df.groupby(['time_scope']).agg(r_count=('rw_spec', count_R), w_count=('rw_spec', count_W))
-        dp.plot.bar()
-
-        if save:
-            plt.savefig('blk_rw_intensive_plot.png')
-        else:
-            plt.show()
+        df = df.groupby(['time_scope']).agg(r_count=('rw_spec', count_R), w_count=('rw_spec', count_W))
+        df.plot.bar(ax=ax[1, 0])
 
     @staticmethod
-    def address_frequency(df, save=True):
+    def address_frequency(df):
         c = Counter()
         _ = df[['scope_start_address', 'scope_end_address']].apply(
             lambda row: c.update(list(range(row[0], row[1] + 1))), axis=1)
 
         scopes, counts = zip(*c.items())
-        plt.bar(scopes, counts)
-
-        if save:
-            plt.savefig('blk_rw_intensive_plot.png')
-        else:
-            plt.show()
+        plt.bar(scopes, counts, ax=ax[1, 1])
 
 
+dp = BLKPlotter.get_features_dataframe()
 
+time_scope_size = int(dp.timestamp.max() // 100)
+address_scope_size = 25 * 1e6
+
+BLKPlotter.address_frequency(dp)
+BLKPlotter.rw_intensive_plot(dp)
+BLKPlotter.pie_plot(dp)
+BLKPlotter.density_on_size(dp)
+fig.savefig('blk-4main-plots.png')
