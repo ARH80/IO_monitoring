b43ae80adb2bc0e197c269abbfee5f2e8573f9f4
Alireza Ghahramani Kure <alireza.ghahramani@digikala.com>
2023-02-02 14:10:33 +0330

    Qre starting. (RUN)

--- scripts/screens-monitoring.sh
+++ scripts/screens-monitoring.sh
@@ -1,7 +1,17 @@
-screen -dmS monitoring-screen-iostat bash -c "time iostat -mx sda 60 > ./iostat.txt"
-screen -dmS monitoring-screen-blktrace bash -c "time blktrace -d /dev/sda -a complete -o - > ./blktrace.txt"
+echo none > /sys/block/nvme0n1/queue/scheduler
+sync
+echo 3 > /proc/sys/vm/drop_caches
 
-$1 $2 $3
+screen -dmS monitoring-screen-iostat bash -c "time iostat -mx /dev/$2 60 > ./iostat.txt"
+screen -dmS monitoring-screen-blktrace bash -c "time blktrace -d /dev/$2 -a complete -o - > ./trace.txt"
+
+$1
 
 screen -XS monitoring-screen-iostat quit
 screen -XS monitoring-screen-blktrace quit
+
+time cat ./trace.txt | blkparse -i - > ./parsed_trace.txt
+
+rm -f ./trace.txt
+
+python3 src/iostat_monitoring/iostat/main.py --data ./iostat.txt --disk $2 --fig-output iostat-plot.png plot
