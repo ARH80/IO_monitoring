eadf5f26cd434cddc4106c623c207e44539dd13d
Alireza Ghahramani Kure <alireza.ghahramani@digikala.com>
2023-02-04 17:42:12 +0330

    Qre(end)

--- src/iostat_monitoring/intensive.py
+++ src/iostat_monitoring/intensive.py
@@ -12,25 +12,26 @@ cpu = df_cpu['%user']
 device = df_devices['%util']
 print(pd.concat([cpu, device], axis=1))
 
-data = [    (dt.datetime(2018, 7, 17, 0, 15), dt.datetime(2018, 7, 17, 0, 30), 'sleep'),
-            (dt.datetime(2018, 7, 17, 0, 30), dt.datetime(2018, 7, 17, 0, 45), 'eat'),
-            (dt.datetime(2018, 7, 17, 0, 45), dt.datetime(2018, 7, 17, 1, 0), 'work'),
-            (dt.datetime(2018, 7, 17, 1, 0), dt.datetime(2018, 7, 17, 1, 30), 'sleep'),
-            (dt.datetime(2018, 7, 17, 1, 15), dt.datetime(2018, 7, 17, 1, 30), 'eat'), 
-            (dt.datetime(2018, 7, 17, 1, 30), dt.datetime(2018, 7, 17, 1, 45), 'work')
-        ]
+data = [
+    (dt.datetime(2018, 7, 17, 0, 15), dt.datetime(2018, 7, 17, 0, 30), 'sleep'),
+    (dt.datetime(2018, 7, 17, 0, 30), dt.datetime(2018, 7, 17, 0, 45), 'eat'),
+    (dt.datetime(2018, 7, 17, 0, 45), dt.datetime(2018, 7, 17, 1, 0), 'work'),
+    (dt.datetime(2018, 7, 17, 1, 0), dt.datetime(2018, 7, 17, 1, 30), 'sleep'),
+    (dt.datetime(2018, 7, 17, 1, 15), dt.datetime(2018, 7, 17, 1, 30), 'eat'),
+    (dt.datetime(2018, 7, 17, 1, 30), dt.datetime(2018, 7, 17, 1, 45), 'work')
+]
 
-cats = {"sleep" : 1, "eat" : 2, "work" : 3}
-colormapping = {"sleep" : "C0", "eat" : "C1", "work" : "C2"}
+cats = {"sleep": 1, "eat": 2, "work": 3}
+colormapping = {"sleep": "C0", "eat": "C1", "work": "C2"}
 
 verts = []
 colors = []
 for d in data:
-    v =  [(mdates.date2num(d[0]), cats[d[2]]-.4),
-          (mdates.date2num(d[0]), cats[d[2]]+.4),
-          (mdates.date2num(d[1]), cats[d[2]]+.4),
-          (mdates.date2num(d[1]), cats[d[2]]-.4),
-          (mdates.date2num(d[0]), cats[d[2]]-.4)]
+    v = [(mdates.date2num(d[0]), cats[d[2]] - .4),
+         (mdates.date2num(d[0]), cats[d[2]] + .4),
+         (mdates.date2num(d[1]), cats[d[2]] + .4),
+         (mdates.date2num(d[1]), cats[d[2]] - .4),
+         (mdates.date2num(d[0]), cats[d[2]] - .4)]
     verts.append(v)
     colors.append(colormapping[d[2]])
 
@@ -39,7 +40,7 @@ bars = PolyCollection(verts, facecolors=colors)
 fig, ax = plt.subplots()
 ax.add_collection(bars)
 ax.autoscale()
-loc = mdates.MinuteLocator(byminute=[0,15,30,45])
+loc = mdates.MinuteLocator(byminute=[0, 15, 30, 45])
 ax.xaxis.set_major_locator(loc)
 ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(loc))
 
