2121430e8328befd2195247a18d1b17303b6de79
ARH80 <ahmadreza1380.hamzei@gmail.com>
2023-02-04 17:42:07 +0330

    intensive

--- src/iostat_monitoring/intensive.py
+++ src/iostat_monitoring/intensive.py
@@ -11,38 +11,6 @@ df_devices = pd.read_csv(sys.argv[2]).set_index('datetime')
 cpu = df_cpu['%user']
 device = df_devices['%util']
 print(pd.concat([cpu, device], axis=1))
-
-data = [    (dt.datetime(2018, 7, 17, 0, 15), dt.datetime(2018, 7, 17, 0, 30), 'sleep'),
-            (dt.datetime(2018, 7, 17, 0, 30), dt.datetime(2018, 7, 17, 0, 45), 'eat'),
-            (dt.datetime(2018, 7, 17, 0, 45), dt.datetime(2018, 7, 17, 1, 0), 'work'),
-            (dt.datetime(2018, 7, 17, 1, 0), dt.datetime(2018, 7, 17, 1, 30), 'sleep'),
-            (dt.datetime(2018, 7, 17, 1, 15), dt.datetime(2018, 7, 17, 1, 30), 'eat'), 
-            (dt.datetime(2018, 7, 17, 1, 30), dt.datetime(2018, 7, 17, 1, 45), 'work')
-        ]
-
-cats = {"sleep" : 1, "eat" : 2, "work" : 3}
-colormapping = {"sleep" : "C0", "eat" : "C1", "work" : "C2"}
-
-verts = []
-colors = []
-for d in data:
-    v =  [(mdates.date2num(d[0]), cats[d[2]]-.4),
-          (mdates.date2num(d[0]), cats[d[2]]+.4),
-          (mdates.date2num(d[1]), cats[d[2]]+.4),
-          (mdates.date2num(d[1]), cats[d[2]]-.4),
-          (mdates.date2num(d[0]), cats[d[2]]-.4)]
-    verts.append(v)
-    colors.append(colormapping[d[2]])
-
-bars = PolyCollection(verts, facecolors=colors)
-
-fig, ax = plt.subplots()
-ax.add_collection(bars)
-ax.autoscale()
-loc = mdates.MinuteLocator(byminute=[0,15,30,45])
-ax.xaxis.set_major_locator(loc)
-ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(loc))
-
-ax.set_yticks([1, 2])
-ax.set_yticklabels(["IO", "CPU"])
+df = pd.concat([cpu, device], axis=1)
+df.plot(title="CPU/IO intensive")
 plt.savefig("intensive.png")
